abeea28ed28802fafe2a964d02674c6981557b1a
KiraFlux <pret2titio1n@gmail.com>
2026-02-09 19:29:39 +0300

    Blue.Task2

--- Lab7/Blue/Task2.cs
+++ Lab7/Blue/Task2.cs
@@ -2,5 +2,39 @@
 {
     public class Task2
     {
+        public struct Participant(string name, string surname) // Тривиальный конструктор потому что можем
+        {
+            const int JumpsTotal = 2;
+            const int JudmentsTotal = 5;
+
+            readonly string name = name, surname = surname;
+            int[,] marks = new int[JumpsTotal, JudmentsTotal];
+            int jumps_done = 0;
+
+            public readonly string Name => name;
+            public readonly string Surname => surname;
+            public readonly int[,]? Marks => marks == null ? null : (int[,])marks.Clone();
+            public readonly int TotalScore => (marks == null) ? 0 : marks.Cast<int>().Sum();
+
+            public void Jump(int[] result)
+            {
+                if (IsResultDestinationValid(result) && CanJump)
+                {
+                    for (int i = 0; i < JudmentsTotal; i += 1) { this.marks[this.jumps_done, i] = result[i]; }
+                    this.jumps_done += 1;
+                }
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array != null) { Array.Sort(array, (l, r) => r.TotalScore.CompareTo(l.TotalScore)); }
+            }
+
+            public void Print() => Console.WriteLine($"Participant{{name: \"{name}\", surname: \"{surname}\", totalScore: {TotalScore}}}");
+
+            bool IsResultDestinationValid(int[] result) => result != null && result.Length == JudmentsTotal;
+
+            readonly bool CanJump => this.jumps_done < JumpsTotal && this.marks != null;
+        }
     }
 }
